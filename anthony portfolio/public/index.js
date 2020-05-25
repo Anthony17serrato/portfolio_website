62aeac89044e9cc5cf3b50c9db9a02bd0852ae3d
anthony17serrato <anthony17@csu.fullerton.edu>
2020-05-24 23:42:29 -0700

    latest changes

--- anthony portfolio/public/index.js
+++ anthony portfolio/public/index.js
@@ -4,3 +4,16 @@ $(".nav-link").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).addClass("active");
 });
+
+const callback = function(entries) {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("is-visible");
+  });
+};
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".show-on-scroll");
+targets.forEach(function(target) {
+  observer.observe(target);
+});
